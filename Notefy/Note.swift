7162cfcf71eebce61a22472a735032682b4e13fa
FarinAlten <122533479+FarinAlten@users.noreply.github.com>
2025-11-02 17:29:41 +0100

    improved the design of the app

--- Notefy/Note.swift
+++ Notefy/Note.swift
@@ -1,18 +1,8 @@
-// Note.swift
-// Notefy
+//
+//  note.swift
+//  Notefy
+//
+//  Created by Farin Altenhöner on 02.11.25.
+//
 
 import Foundation
-
-struct Note: Identifiable, Codable, Hashable {
-    let id: UUID
-    var title: String
-    var content: String
-    var createdAt: Date
-
-    init(id: UUID = UUID(), title: String = "", content: String = "", createdAt: Date = Date()) {
-        self.id = id
-        self.title = title
-        self.content = content
-        self.createdAt = createdAt
-    }
-}
