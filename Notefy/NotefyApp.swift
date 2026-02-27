e2a011cb3b97f53cfc94a2b13bdf9e7a5abdd003
FarinAlten <122533479+FarinAlten@users.noreply.github.com>
2026-02-27 17:57:40 +0100

    Initial Commit

---
+++ Notefy/NotefyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotefyApp.swift
+//  Notefy
+//
+//  Created by Farin Altenhöner on 27.02.26.
+//
+
+import SwiftUI
+
+@main
+struct NotefyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
